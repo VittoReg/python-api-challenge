529062cebdbec0d838f49a85f9d792a8f650ca39
VittoReg <157906976+VittoReg@users.noreply.github.com>
2024-06-17 19:35:55 +0100

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "3f08c3195dc2050fc9dc60d18071a5a8"
+
+# Geoapify API Key
+geoapify_key = "67aa3bb693234c1385c195fd3147b02c"
---
+++ weather_api_key.py
@@ -0,0 +1,2 @@
+# OpenWeatherMap API Key
+weather_api_key = "3f08c3195dc2050fc9dc60d18071a5a8"
